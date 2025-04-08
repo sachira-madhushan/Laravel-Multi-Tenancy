333b88d75b4fbb6e9f348f1e88bd3c823a2a88f5
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-04-08 11:30:05 +0530

    Add relevant user ID when model boot

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -22,7 +22,10 @@ class ProjectController extends Controller
 
     public function store(StoreProjectRequest $request)
     {
-        Project::create($request->validated()+['user_id' => auth()->id()]);
+        Project::create(
+            $request->validated()
+            //+['user_id' => auth()->id()]
+        );
 
         return redirect()->route('projects.index');
     }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,4 +9,16 @@ class Project extends Model
     protected $fillable = [
         'name','user_id'
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::creating(
+            function ($model) {
+                $model->user_id = auth()->id();
+            }
+        );
+    }
+    
 }
